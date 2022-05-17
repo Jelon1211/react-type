7114092f4fa6e689ec60ebe8162db5a93b91666a
jelon1211 <jelongames@gmail.com>
2022-05-17 22:14:00 +0200

    topbar Finished

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -1,5 +1,34 @@
-import React from "react";
+import "./topbar.css";
+import { NotificationsNone, Language, Settings } from "@mui/icons-material";
 
-export default function Topbar() {
-  return <div>Topbar</div>;
-}
+const Topbar = () => {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <div className="logo">Jelon-admin</div>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBag">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBag">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img
+            src="https://randomwordgenerator.com/img/picture-generator/55e6dd474b5baa14f1dc8460962e33791c3ad6e04e5074417c2d78d19f48c3_640.jpg"
+            alt=""
+            className="topAvatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
