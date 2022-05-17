0799ac769c502fa0bc21d06e7d76b465c7e41614
jelon1211 <jelongames@gmail.com>
2022-05-17 10:45:09 +0200

    c4

--- src/component.tsx
+++ src/component.tsx
@@ -10,7 +10,7 @@ const Count = ({ initCount }: any) => {
   const decrement = () => {
     setCount((currCount: number) => currCount - 1);
   };
-  // comment 3
+  // comment 4
   return (
     <div>
       <button onClick={decrement}>-</button>
